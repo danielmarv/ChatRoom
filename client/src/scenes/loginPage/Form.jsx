a612ba82940854d9394ab16686d66b917d9d730e
Daniel Ntege <danientege785@gmail.com>
2023-11-17 13:52:59 +0300

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://chatroomapi-v1.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
